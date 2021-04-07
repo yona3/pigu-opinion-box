b3e7194011c246e0903e530020f57fae461c3e0c
yona3 <kou.y.0905@icloud.com>
2021-04-07 18:32:58 +0900

    🎉 init

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>pigu｜ご意見箱</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1 className="text-3xl text-center mt-12">pigu｜ご意見箱</h1>
+      </main>
+    </div>
+  );
+}
