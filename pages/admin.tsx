a45cc61dd570c6d4441115efb9814f093c8b2656
yona3 <kou.y.0905@icloud.com>
2021-04-13 19:31:06 +0900

    🔒 Fix Admin page visible

--- pages/admin.tsx
+++ pages/admin.tsx
@@ -1,4 +1,20 @@
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+import { useAuthentication } from '../hooks/authentication';
+
 export default function Admin(): JSX.Element {
+  const router = useRouter();
+  const { user } = useAuthentication();
+
+  useEffect(() => {
+    if (
+      !user ||
+      user.uid !== process.env.NEXT_PUBLIC_FIREBASE_ADMIN_UID ||
+      user.isAnonymous
+    )
+      router.push('/');
+  }, [user]);
+
   return (
     <div>
       <h1>admin page</h1>
