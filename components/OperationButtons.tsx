602f806de28d7fdfe07b0df5d81fd59033ee1324
yona3 <kou.y.0905@icloud.com>
2021-04-16 17:25:26 +0900

    🔨 Update share URL & buttons style

--- components/OperationButtons.tsx
+++ components/OperationButtons.tsx
@@ -1,6 +1,8 @@
 import Link from 'next/link';
 
 export const OperationButtons = ({ isReplied, setIsReplied }) => {
+  const siteUrl = 'https://pigu-opinion-box.vercel.app';
+
   const deleteOpinion = () => {
     const result = confirm('削除しますか？');
 
@@ -18,25 +20,25 @@ export const OperationButtons = ({ isReplied, setIsReplied }) => {
   return (
     <div className="mt-8">
       <a
-        href={`https://twitter.com/intent/tweet?=&text=ご意見箱🗳%0a%0a%23pigu%20%23piguご意見箱`}
+        href={`https://twitter.com/intent/tweet?url=${siteUrl}&text=ご意見箱🗳%0a%0a%23pigu%20%23piguご意見箱%0A`}
         target="_blank"
         rel="noopener noreferrer"
       >
-        <button className="border text-sm border-blue-300 text-blue-500 hover:bg-blue-100 transition rounded-md w-full py-2.5">
+        <button className="text-sm focus:outline-none bg-blue-500 text-white hover:bg-blue-400 transition rounded-md w-full py-3">
           Twitterへ移動
         </button>
       </a>
 
-      <div className="flex mt-2 space-x-2">
+      <div className="flex mt-2 space-x-1">
         <button
-          className="border text-sm w-full border-green-300 text-green-500 hover:bg-green-100 transition rounded-md py-2.5"
+          className="border focus:outline-none text-sm w-full border-green-300 text-green-500 hover:bg-green-100 transition rounded-md py-2.5"
           onClick={handleChangeIsReplied}
         >
           {isReplied ? '未回答にする' : '回答済みにする'}
         </button>
 
         <button
-          className="border text-sm border-red-300 text-red-500 hover:bg-red-100 transition rounded-md w-full py-2.5"
+          className="border focus:outline-none text-sm border-red-300 text-red-500 hover:bg-red-100 transition rounded-md w-full py-2.5"
           onClick={deleteOpinion}
         >
           削除する
@@ -45,7 +47,7 @@ export const OperationButtons = ({ isReplied, setIsReplied }) => {
 
       <Link href="/admin">
         <a className="block mt-2">
-          <button className="border text-sm border-gray-300 text-gray-800 hover:bg-gray-100 transition rounded-md w-full py-2.5">
+          <button className="border focus:outline-none text-sm border-gray-300 text-gray-800 hover:bg-gray-100 transition rounded-md w-full py-2.5">
             もどる
           </button>
         </a>
